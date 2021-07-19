5ac46d5b19143f977c0e9049c19a0607f8fe832b
Yoga Pranata <ahmadzaylani001@gmail.com>
2021-07-19 22:13:24 +0700

    Update userinfo.py

--- LaylaRobot/modules/userinfo.py
+++ LaylaRobot/modules/userinfo.py
@@ -312,7 +312,7 @@ def info(update: Update, context: CallbackContext):
         disaster_level_present = True
 
     if disaster_level_present:
-        text += ' [<a href="https://t.me/YBotsSupport">Levelling</a>]'.format(
+        text += ' [<a href="https://t.me/asunalogs/24">Levelling</a>]'.format(
             bot.username
         )
 
@@ -485,7 +485,7 @@ def set_about_bio(update: Update, context: CallbackContext):
 
         if user_id == bot.id and sender_id not in DEV_USERS:
             message.reply_text(
-                "Erm... yeah, I only trust Heroes Association to set my bio."
+                "Erm... yeah, I only trust Spread Networks Member to set my bio."
             )
             return
 
