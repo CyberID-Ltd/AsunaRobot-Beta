1287b1a948d9b0a8591fc6e754dc6a9a5c377c96
Yoga Pranata <ahmadzaylani001@gmail.com>
2021-07-21 02:13:03 +0700

    Update userinfo.py

--- LaylaRobot/modules/userinfo.py
+++ LaylaRobot/modules/userinfo.py
@@ -293,26 +293,26 @@ def info(update: Update, context: CallbackContext):
     disaster_level_present = False
 
     if user.id == OWNER_ID:
-        text += "\n\nThe Disaster level of this person is 'Master'."
+        text += "\n\nThis user is my `Husband` or my `Master`."
         disaster_level_present = True
     elif user.id in DEV_USERS:
-        text += "\n\nThis user is member of 'Spread Networks'."
+        text += "\n\nThis user is one of the `Developer`."
         disaster_level_present = True
     elif user.id in DRAGONS:
-        text += "\n\nThe Disaster level of this person is 'Dragon'."
+        text += "\n\nThe Disaster level of this person is `Dragon`."
         disaster_level_present = True
     elif user.id in DEMONS:
-        text += "\n\nThe Disaster level of this person is 'Demon'."
+        text += "\n\nThe Disaster level of this person is `Demon`."
         disaster_level_present = True
     elif user.id in TIGERS:
-        text += "\n\nThe Disaster level of this person is 'Tiger'."
+        text += "\n\nThe Disaster level of this person is `Tiger`."
         disaster_level_present = True
     elif user.id in WOLVES:
-        text += "\n\nThe Disaster level of this person is 'Wolf'."
+        text += "\n\nThe Disaster level of this person is `Wolf`."
         disaster_level_present = True
 
     if disaster_level_present:
-        text += ' [<a href="https://t.me/asunalogs/24">Levelling</a>]'.format(
+        text += ' [<a href="https://t.me/asunalogs/24">Levelling Details</a>]'.format(
             bot.username
         )
 
