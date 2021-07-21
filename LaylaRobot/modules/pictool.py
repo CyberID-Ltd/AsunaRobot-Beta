c517a127438eec5da077b42d3cb6f95771578b60
Yoga Pranata <ahmadzaylani001@gmail.com>
2021-07-21 18:50:29 +0700

    Create pictool.py

---
+++ LaylaRobot/modules/pictool.py
@@ -0,0 +1,13 @@
+__mod_name__ = "Pict-Tool"
+
+__help__ = """
+*Remove background*
+ ❍ /rmbg : Remove background your picture using remove.bg tools.
+
+*Logo Maker*
+ ❍ /logo text :  Create your logo with your name.
+ ❍ /wlogo text :  Create your logo with your name.
+
+*Do not remove this if you respect on me.*
+© 2021 @SpreadNetworks
+"""
