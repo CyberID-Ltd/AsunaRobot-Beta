3857f810e2d28e880b842dffc4cbeb0583f79b21
Yoga Pranata <ahmadzaylani001@gmail.com>
2021-07-21 16:35:38 +0700

    Update __tools.py

--- LaylaRobot/modules/__tools.py
+++ LaylaRobot/modules/__tools.py
@@ -2,29 +2,35 @@ __mod_name__ = "Tools"
 
 __help__ = """
 *Fake Info*
- ❍ /fakegen*:* Generates Fake Information
- ❍ /picgen*:* Generate a fake pic 
+ ❍ /fakegen *:* Generates Fake Information
+ ❍ /picgen *:* Generate a fake pic 
 
 *Date-time-Weather*
- ❍ /time <country code>*:* Gives information about a timezone.
- ❍ /weather <city>*:* Get weather info in a particular place.
- ❍ /wttr <city>*:* Advanced weather module, usage same as /weather
- ❍ /wttr moon*:* Get the current status of moon
+ ❍ /time <country code> *:* Gives information about a timezone.
+ ❍ /weather <city> *:* Get weather info in a particular place.
+ ❍ /wttr <city> *:* Advanced weather module, usage same as /weather
+ ❍ /wttr moon *:* Get the current status of moon
  
 *Quotly*
- ❍ /q : reply any message with this command to create a sticker.
+ ❍ /q *:* reply any message with this command to create a sticker.
 
 *Phone Info*
- ❍ /phone `<full phone number>` : Get details of phone number.
+ ❍ /phone `<full phone number>` *:* Get details of phone number.
 
 *JSON*
- ❍ /json*:* Get Detailed info about any message
+ ❍ /json *:* Get Detailed info about any message
 
-*Encryptor*
- ❍ /encrypt*:* Encrypts The Given Text
- ❍ /decrypt*:* Decrypts Previously Ecrypted Text
+*Encryp Decrypt*
+ ❍ /encrypt *:* Encrypts The Given Text
+ ❍ /decrypt *:* Decrypts Previously Ecrypted Text
+
+*Website Screenshoot*
+ ❍ /webss [URL] *:* Take A Screenshot Of A Webpage
 
 *Zipper*
- ❍ /zip*:* reply to a telegram file to compress it in .zip format
- ❍ /unzip*:* reply to a telegram file to decompress it from the .zip format
+ ❍ /zip *:* reply to a telegram file to compress it in .zip format
+ ❍ /unzip *:* reply to a telegram file to decompress it from the .zip format
+
+*Do not remove this if you respect us*
+ⓒ @RxyMX & @Yoga_CIC
 """
